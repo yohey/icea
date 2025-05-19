03c2955a8e614a57d77ed77038be35b97da814e9
Yohey Ishizu <yohey.ishizu@gmail.com>
2025-05-19 22:31:59 +0900

    ライブラリ探索結果がフルパスでなかった場合への対処

--- src/interfaces/python/icea/_findlib.py
+++ src/interfaces/python/icea/_findlib.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-_libpath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+_libpath = os.path.dirname(os.path.dirname(__file__))
 
 if sys.platform == 'linux':
     if 'LD_LIBRARY_PATH' in os.environ:
@@ -35,6 +35,25 @@ _libceapath = find_library('cea')
 
 if _libceapath is None:
     raise RuntimeError('Library is not found.')
+
+if not os.path.isabs(_libceapath):
+    if sys.platform == 'linux':
+        for d in os.environ['LD_LIBRARY_PATH'].split(':'):
+            if os.path.exists(os.path.join(d, _libceapath)):
+                _libceapath = os.path.join(d, _libceapath)
+                break
+
+    elif sys.platform == 'darwin':
+        for d in os.environ['DYLD_LIBRARY_PATH'].split(':'):
+            if os.path.exists(os.path.join(d, _libceapath)):
+                _libceapath = os.path.join(d, _libceapath)
+                break
+
+    elif sys.platform == 'win32':
+        for d in os.environ['PATH'].split(';'):
+            if os.path.exists(os.path.join(d, _libceapath)):
+                _libceapath = os.path.join(d, _libceapath)
+                break
 
 
 if sys.platform == 'win32':
